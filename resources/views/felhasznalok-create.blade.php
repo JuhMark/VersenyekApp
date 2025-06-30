971d46713c4c2a9e786253b0aa2c893f474011da
Juhász Márk <jmark4@freemail.hu>
2025-06-30 22:48:11 +0200

    Removed cancel button

--- resources/views/felhasznalok-create.blade.php
+++ resources/views/felhasznalok-create.blade.php
@@ -51,7 +51,6 @@
     @endif
   </div>
   <div class="mt-6 flex items-center justify-end gap-x-6">
-    <button type="button" class="text-sm/6 font-semibold text-gray-900">Mégse</button>
     <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Mentés</button>
   </div>
 </form>
